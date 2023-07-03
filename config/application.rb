717a8e3a2756e60785657c0337beda05073c2d9c
yoshi-non <himekawa.yoshino44@gmail.com>
2023-07-04 07:55:18 +0900

    add ログイン処理

--- config/application.rb
+++ config/application.rb
@@ -18,17 +18,17 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Backend
+module HalCinemaApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
-
+    # config.load_defaults 7.0
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Only loads a smaller set of middleware suitable for API only apps.
