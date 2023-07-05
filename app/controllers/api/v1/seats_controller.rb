6416e722439606700bc454d5b960ce76d0c7a816
yoshi-non <himekawa.yoshino44@gmail.com>
2023-07-05 18:40:39 +0900

    add 座席情報API

---
+++ app/controllers/api/v1/seats_controller.rb
@@ -0,0 +1,54 @@
+module Api
+    module V1
+      class SeatsController < ApplicationController
+        before_action :set_seat, only: [:show, :update, :destroy]
+  
+        # GET /api/v1/seats
+        def index
+          @seats = Seat.all
+          render json: @seats
+        end
+  
+        # GET /api/v1/seats/:id
+        def show
+          render json: @seat
+        end
+  
+        # POST /api/v1/seats
+        def create
+          @seat = Seat.new(seat_params)
+  
+          if @seat.save
+            render json: @seat, status: :created
+          else
+            render json: @seat.errors, status: :unprocessable_entity
+          end
+        end
+  
+        # PATCH/PUT /api/v1/seats/:id
+        def update
+          if @seat.update(seat_params)
+            render json: @seat
+          else
+            render json: @seat.errors, status: :unprocessable_entity
+          end
+        end
+  
+        # DELETE /api/v1/seats/:id
+        def destroy
+          @seat.destroy
+          head :no_content
+        end
+  
+        private
+  
+        def set_seat
+          @seat = Seat.find(params[:id])
+        end
+  
+        def seat_params
+          params.require(:seat).permit(:screen_id, :column, :row)
+        end
+      end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       resources :movies
       resources :tickets
       resources :screens
+      resources :seats
     end
   end
 end
