3e7c947e87bf268812ef34d1e362fa58e1fae020
yoshi-non <himekawa.yoshino44@gmail.com>
2023-07-05 18:07:05 +0900

    add チケット情報API

--- sql/hal_cinema.sql
+++ sql/hal_cinema.sql
@@ -4,15 +4,18 @@ BEGIN;
 
 -- Movie
 -- DELETE FROM movies;
-INSERT INTO movies (title, description, director, actors, duration, thumbnail, created_at, updated_at) VALUES ('テストタイトル映画', '説明文説明文説明文説明文説明文説明文', '監督', '出演者', 150, 'サムネイル画像のパス', current_timestamp, current_timestamp);
+-- INSERT INTO movies (title, description, director, actors, duration, thumbnail, created_at, updated_at) VALUES ('テストタイトル映画', '説明文説明文説明文説明文説明文説明文', '監督', '出演者', 150, 'サムネイル画像のパス', current_timestamp, current_timestamp);
 
 -- DELETE FROM screens;
--- insert into screens (name, created_at, updated_at) values ("スクリーン1", current_timestamp, current_timestamp);
+-- INSERT INTO screens (name, created_at, updated_at) VALUES ('スクリーン1', current_timestamp, current_timestamp);
 
 -- DELETE FROM seats;
--- insert into seats (column, row, created_at, updated_at) values ("A", 1, current_timestamp, current_timestamp);
+-- INSERT INTO seats (column, row, created_at, updated_at) VALUES ('A', 1, current_timestamp, current_timestamp);
 
 -- DELETE FROM tickets;
--- insert into tickets (name, price, created_at, updated_at) values ("一般", 1800, current_timestamp, current_timestamp);
+-- INSERT INTO tickets (name, price, created_at, updated_at) VALUES ('一般', 1800, current_timestamp, current_timestamp);
+-- INSERT INTO tickets (name, price, created_at, updated_at) VALUES ('大学生等', 1600, current_timestamp, current_timestamp);
+-- INSERT INTO tickets (name, price, created_at, updated_at) VALUES ('中学、高校', 1400, current_timestamp, current_timestamp);
+-- INSERT INTO tickets (name, price, created_at, updated_at) VALUES ('小学生、幼児', 1000, current_timestamp, current_timestamp);
 
 COMMIT;
