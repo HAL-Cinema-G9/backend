120d87eef1a1d79535d3062ec07935d6952f61da
yoshi-non <himekawa.yoshino44@gmail.com>
2023-07-07 07:06:51 +0900

    add 自身のUserデータの取得

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post 'auth/:provider/callback', to: 'users#create'
+      resource :user, only: [:show]
       resources :movies
       resources :tickets
       resources :screens
